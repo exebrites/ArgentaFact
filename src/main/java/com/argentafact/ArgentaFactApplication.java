a0ec38ca7d4d4e3ef9d2286f3eb2abf81428420f
exe <exequielbrites8@gmail.com>
2026-01-09 19:21:56 -0300

    firts commit

---
+++ src/main/java/com/argentafact/ArgentaFactApplication.java
@@ -0,0 +1,13 @@
+package com.argentafact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgentaFactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgentaFactApplication.class, args);
+	}
+
+}
