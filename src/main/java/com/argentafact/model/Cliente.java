56faffd745aa83c7dca32172c943bce023b2e466
exe <exequielbrites8@gmail.com>
2026-01-12 08:52:08 -0300

    feat:model cliente

---
+++ src/main/java/com/argentafact/model/Cliente.java
@@ -0,0 +1,36 @@
+package com.argentafact.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    
+    public Cliente() {
+    }
+    public Cliente(Long id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
