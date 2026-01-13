006017befceaba324827fcbf9c2d511d8ff944df
exe <exequielbrites8@gmail.com>
2026-01-13 15:01:27 -0300

    feat:cambio index.html por dashboard.html

--- src/main/java/com/argentafact/controller/HomeController.java
+++ src/main/java/com/argentafact/controller/HomeController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String hola() {
-        return "index";
+    public String administracion() {
+        return "dashboard";
         
     }
 }
