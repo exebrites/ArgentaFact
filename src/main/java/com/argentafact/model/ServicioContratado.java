15ddd160bc0048644726bb29b9304b6f512445e2
exe <exequielbrites8@gmail.com>
2026-01-29 17:04:45 -0300

    feat:creacion de la entidad servicioContratado asociado con cliente y servicio

--- src/main/java/com/argentafact/model/ServicioContratado.java
+++ src/main/java/com/argentafact/model/ServicioContratado.java
@@ -1,8 +1,25 @@
 package com.argentafact.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "servicios_contratados")
 public class ServicioContratado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne
+    @JoinColumn(name="id_cliente", nullable=false)
     private Cliente cliente;
+
+    @ManyToOne
+    @JoinColumn(name="id_servicio", nullable=false)
     private Servicio servicio;
     
     public ServicioContratado(){
