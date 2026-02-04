e5fc88cf49d079a85b2d8bce44e3dcbdb93d0103
exe <exequielbrites8@gmail.com>
2026-02-04 14:02:56 -0300

    respositorio empleado

--- src/main/java/com/argentafact/repository/EmpleadoRepository.java
+++ src/main/java/com/argentafact/repository/EmpleadoRepository.java
@@ -1,5 +1,7 @@
 package com.argentafact.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.argentafact.model.Empleado;
@@ -8,5 +10,11 @@ public interface EmpleadoRepository extends JpaRepository<Empleado, Long> {
 
     Empleado findByIdEmpleado(Long id);
 	
- 
+     boolean existsByDni(String dni);
+    
+    boolean existsByEmail(String email);
+    
+    Optional<Empleado> findByDni(String dni);
+    
+    Optional<Empleado> findByEmail(String email);
 }
--- src/main/java/com/argentafact/repository/UsuarioRepository.java
+++ src/main/java/com/argentafact/repository/UsuarioRepository.java
@@ -16,4 +16,6 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     boolean existsByUsername(String username);
 
     boolean existsByEmail(String email);
+
+
 }
