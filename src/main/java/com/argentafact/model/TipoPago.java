c2b94fd236aee56fabbce752d89b8188a85cf955
franklin <francolcrive@gmail.com>
2026-02-05 12:30:53 -0300

    Corrección en TipoPago.java para la vista del medio de pago

--- src/main/java/com/argentafact/model/TipoPago.java
+++ src/main/java/com/argentafact/model/TipoPago.java
@@ -1,10 +1,20 @@
 package com.argentafact.model;
 
 public enum TipoPago {
-    EFECTIVO,
-    TRANSFERENCIA,
-    TARJETA_CREDITO,
-    TARJETA_DEBITO,
-    BILLETERA_VIRTUAL
-}
 
+    EFECTIVO("Efectivo"),
+    TRANSFERENCIA("Transferencia"),
+    TARJETA_CREDITO("Tarjeta Crédito"),
+    TARJETA_DEBITO("Tarjeta Débito"),
+    BILLETERA_VIRTUAL("Billetera Virtual");
+
+    private final String descripcion;
+
+    TipoPago(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+}
